3daa91a66de759cad16b574256b589c5f066fee9
tusharban <tushar.agrawal9217@gmail.com>
2025-08-09 14:50:26 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,5 +3,6 @@ api_hash = "0e388d780696b99dc38fc078cbaf404f" #change_with_your_Api_hash.
 bot_token = "7913081252:AAEvRz9FIqZFjIplkgUApYZ9Cm4-h0qQWfc" #change_with_your_bot_token.
 auth_users = [1346074645] #change_with_your_user_id.
 sudo_user = 1346074645 #change_with_your_user_id.
-log_channel = -1002524642209 #change_with_your_channel_id.
-txt_channel = -1002524642209 #change_with_your_channel_id.
+log_channel = -1002604144654 #change_with_your_channel_id.
+txt_channel = -1002604144654 #change_with_your_channel_id.
+
